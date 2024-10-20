d2286d3b8b6f89f2633c5ba53c4b275e48a96e68
Scott Wu <sw@scottwu.ca>
2024-10-20 23:31:40 +0800

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import * as Select from "$lib/components/ui/select/index.js";
+
+  const fruits = [
+    { value: "apple", label: "Apple" },
+    { value: "banana", label: "Banana" },
+    { value: "blueberry", label: "Blueberry" },
+    { value: "grapes", label: "Grapes" },
+    { value: "pineapple", label: "Pineapple" },
+  ];
+
+  let selected;
+</script>
+
+<Select.Root bind:selected>
+  <Select.Trigger class="w-[180px]" bind:selected>
+    <Select.Value placeholder="Select a fruit" />
+  </Select.Trigger>
+  <Select.Content>
+    <Select.Group>
+      <Select.Label>Fruits</Select.Label>
+      {#each fruits as fruit}
+        <Select.Item value={fruit.value} label={fruit.label}
+          >{fruit.label}</Select.Item
+        >
+      {/each}
+    </Select.Group>
+  </Select.Content>
+  <Select.Input name="favoriteFruit" />
+</Select.Root>
